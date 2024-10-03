43af92ab13da9b7d6adf503a300ba5284bfbc51e
Davi Ruas <daviruasfb@gmail.com>
2024-10-03 15:15:21 -0300

    Fixing bug related to api file

--- spotifyback/app.py
+++ spotifyback/app.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, redirect, request, jsonify, make_response, session, url_for
-from utils.spotify import *
 import spotipy
+from flask import Flask, render_template, redirect, session
+from utils.spotify import *
+from utils.DownloadVideos import *
+from dotenv import load_dotenv
 
-import os
-
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'a1b2d3e4c5f6'
-app.config['SESSION_COOKIE_NAME'] = 'spotify-login'
-TOKEN_INFO = "token_info"
+app.secret_key = os.getenv("SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = os.getenv("SESSION_COOKIE_NAME")
+TOKEN_INFO = os.getenv("TOKEN_INFO")
 
 @app.route('/')
 def login():
@@ -63,7 +64,7 @@ def submit_selected_songs():
 
     path = "Data/selected_songs.csv"
     save_songs_csv(selected_songs, path)
-    print('Selected songs received')
+    return video_dowload(selected_songs)
 
 
 if __name__ == '__main__':
